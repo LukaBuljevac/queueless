199f1a07715bb02b462c4f8e2e82a6af15148109
LukaBuljevac <buljevacluka@gmail.com>
2026-01-08 11:59:05 +0100

    Fix: urls.py file

--- appointments/urls.py
+++ appointments/urls.py
@@ -7,12 +7,18 @@ from .views_booking import book_appointment
 from .views_client import my_appointments
 
 urlpatterns = [
+    # CLIENT
     path("client/", client_dashboard, name="client_dashboard"),
-    path("staff/", staff_dashboard, name="staff_dashboard"),
-    path("admin/", admin_dashboard, name="admin_dashboard"),
+    path("client/book/", book_appointment, name="book_appointment"),
+    path("client/my/", my_appointments, name="my_appointments"),
 
+    # STAFF
+    path("staff/", staff_dashboard, name="staff_dashboard"),
     path("staff/availability/", availability_list, name="availability_list"),
     path("staff/availability/new/", availability_create, name="availability_create"),
     path("staff/availability/<int:pk>/edit/", availability_update, name="availability_update"),
     path("staff/availability/<int:pk>/delete/", availability_delete, name="availability_delete"),
+
+    # ADMIN
+    path("admin/", admin_dashboard, name="admin_dashboard"),
 ]
